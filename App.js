18927e4a9dd1de7c85ce9365d6d2889b6b5cf40f
Misha <misharexviashvili@yahoo.com>
2023-01-29 23:42:39 +0400

    Navigation added

--- App.js
+++ App.js
@@ -1,11 +1,18 @@
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { Fragment } from "react";
 import { StatusBar, StyleSheet } from "react-native";
 import CategoriesScreen from "./screens/CategoriesScreen";
+const Stack = createNativeStackNavigator();
 export default function App() {
   return (
     <Fragment>
-      <StatusBar style="light" />                         
-      <CategoriesScreen />
+      <StatusBar style="light" />
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen name="MealsCategories" component={CategoriesScreen} />
+        </Stack.Navigator>
+      </NavigationContainer>
     </Fragment>
   );
 }
